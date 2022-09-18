79e8199fa37b63d7576e030ab78304c3accf8596
sabino flores <flores.onibas@gmail.com>
2022-09-18 18:29:36 -0400

    sumar

--- src/suma/Suma.java
+++ src/suma/Suma.java
@@ -4,7 +4,10 @@ public class Suma {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HOLA COMO ESTAN");
+        int a=5;
+        int b=4;
+        int s=a+b;
+        System.out.println("la suma ES  "+  s);
     }
 
 }
