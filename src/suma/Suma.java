6c3ada4d5d6aee5212a4fd90ae1a0bb2828c18b6
sabino flores <flores.onibas@gmail.com>
2022-09-18 17:50:18 -0400

    suma

---
+++ src/suma/Suma.java
@@ -0,0 +1,12 @@
+
+package suma;
+
+
+public class Suma {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA COMO ESTAN");
+    }
+    
+}
